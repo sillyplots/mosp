1e5f21f27778ded41b2ad8c1a8277e2f24bdf6ba
chuck <cthomp870@gmail.com>
2025-11-28 11:55:34 -0800

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,9 @@ export function Footer() {
           &copy; ${year} Ministry of Silly Plots. All rights reserved, unfortunately.
         </p>
         <p style="font-size: 0.8rem; margin-top: var(--spacing-sm); font-style: italic;">
-          "What I want is wasting your time and mine."
+          "I don't want fancy things - or fancy schmancy things. I don't even want fancy schmancy wancy things, or fancy schmancy take a trip to Francey things. What I want is wasting your time and mine."
         </p>
       </div>
     </footer>
   `;
 }
-
-"I don't want fancy things - or fancy schmancy things. I don't even want fancy schmancy wancy things, or fancy schmancy take a trip to Francey things. What I want is wasting your time and mine"
